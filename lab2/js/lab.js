141848efb91e494eb409d999a3c5e9741d6f7fb2
Chimera-Hills <kimischimera@gmail.com>
2023-10-12 20:17:05 -0700

    CSS Changes

--- lab2/js/lab.js
+++ lab2/js/lab.js
@@ -6,7 +6,7 @@
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
+// this is an example function and this comment tells what it does and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
   // return results;
