2441ba8867a8c55e19f1f16cacdc89a3dba9116c
Chimera-Hills <kimischimera@gmail.com>
2023-10-26 21:20:25 -0700

    lab 6 changes

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,28 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Using arrays and objects to output my main imaginary ride and other modes of transport
+// Author: Kevin Hong
+// Date: 26 Oct. 2023
+
+// Define variables in an array for my modes of transport
+var myTransport = [
+  "bus",
+  "car",
+  "walking"
+]
+
+// Define an object for my main ride
+var myMainRide = {
+  make: "Cadillac",
+  model: "CT5-V Blackwing",
+  color: "Dark Emerald Frost",
+  year: 2022,
+  age: function() {
+      return 2023 - this.year;
+  }
+}
+
+// Output
+document.writeln("My main modes of transportation for moving around UCSC's big campus are " + myTransport[0] + "es, " + myTransport[1] + "s, or " + myTransport[2] + ". <br>");
+document.writeln("The specs for a car that is on my wishlist are: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre>");
 
 // Constants
 
