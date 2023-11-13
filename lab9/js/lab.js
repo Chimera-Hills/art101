41fdf192f3ad8cf55c3d265403f2920255cbcc73
Chimera-Hills <kimischimera@gmail.com>
2023-11-12 17:56:49 -0800

    lab 9 changes

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,15 +1,22 @@
-// index.js - 
+// index.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page.
 // Author: Kevin Hong
 // Date: 6 November, 2023
 
-// Variables
+// Create buttons for each section: Challenges, Problems, & Results
+$(".major-section").append("<br><button id='morph-sections'>Beautify</button>");
 
+// Change the buttons' appearances when the mouse enters it
+$(".major-section button").mouseenter(function () {
+    $(this).addClass("special2");
+});
 
+// Reverts it back once mouse exits
+$(".major-section button").mouseleave(function () {
+    $(this).removeClass("special2");
+});
 
-// Functions
-
-
-
-// Output
-
+// Changes the section's appearance in which a button is in
+$(".major-section button").click(function() {
+    ($(this).parent()).toggleClass("special");
+})
 
