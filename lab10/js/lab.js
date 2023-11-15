240b640acf69eaaf9fe5d05afbc2c5b35c7974e4
Chimera-Hills <kimischimera@gmail.com>
2023-11-14 22:01:29 -0800

    lab 10 changes

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,5 +1,33 @@
-// index.js - 
+// index.js - This simple JavaScript/jQuery script appends new elements to an output div
 // Author: Kevin Hong
 // Date: 14 November, 2023
 
+// Generates random text messages that respond to each other in the output
+function generateRandomText() {
+    // Create a constant for placeholder text, Lorem Ipsum
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    
+    // Set a minimum and maximum length for the generated text
+    const min = 3;
+    const max = 100;
 
+    // Generate a random length between the minimum and maximum number
+    // Rounds the number out
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    // Get a random starting index to slice the Lorem Ipsum text
+    // Rounds the number out
+     const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+
+    // Generate the random Lorem Ipsum-like text
+     return text.slice(randStart, randStart + randLen);
+}
+
+// Create an event listener for the button
+$("#make-convo").click(function(){
+    // Grab new dialogue using previously created function, generateRandomText()
+    const newText = generateRandomText();
+
+    // Append a new div with the class, text, to div with id, output
+    $("#output").append("<div class='text'><p>" + newText + "</p></div>");
+});
